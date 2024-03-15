305184135d3fa57de091817afd54fae98b7f757d
HassanAliJunejo <160749752+HassanAliJunejo@users.noreply.github.com>
2024-03-15 14:32:50 +0500

    Update main.ts

---
+++ main.ts
@@ -0,0 +1,17 @@
+let username = "Hassan ali"
+console.log(username)
+
+let fruit:string = "mango"
+console.log(fruit)
+
+const bankAccount:number = 1223333;
+console.log(bankAccount);
+
+let user_email:any = "user1@gmail.com";
+console.log(user_email);
+
+function Add(){
+        fruit="Apple";
+}
+Add();
+console.log(fruit);
